5e230f0319bead91cfc984b094d5610982227d4d
Frendy Santoso <frendysantoso0098@gmail.com>
2019-06-30 08:04:16 +0700

    Update login.php

--- login.php
+++ login.php
@@ -10,9 +10,23 @@
 	<span style="color: red; font-style: italic;"><?= $_GET['gagal']; ?></span>
 	<?php endif ?>
 	<form action="action_login.php" method="POST">
-		Username : <input type="text" name="username"> <br>
-		Password : <input type="password" name="password"> <br>
-		<button type="submit" name="tombol">Login</button>
+		<table>
+			<tr>
+				<td>Username</td>
+				<td>:</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td>:</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td><button type="submit" name="tombol">Login</button></td>
+			</tr>
+		</table>
 	</form>
 </body>
 </html>
